54d3c65a7960d5436db2e1e6472e1f312fa5b42c
nirnay <nirnaykorde@gmail.com>
2024-01-10 17:18:01 +0530

    Mk1.3

--- src/app/form-module/form-list/form-list.component.ts
+++ src/app/form-module/form-list/form-list.component.ts
@@ -1,16 +1,35 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormMasterService } from '../../form-master.service';
 import { FormFormat } from '../form-format';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-form-list',
   templateUrl: './form-list.component.html',
   styleUrl: './form-list.component.scss',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      ),
+    ]),
+  ],
 })
 export class FormListComponent implements OnInit {
   formList: FormFormat[];
   formService: FormMasterService = inject(FormMasterService);
-  displayedColumns: string[] = ['id', 'name', 'type', 'required'];
+  columnsToDisplay: string[] = ['name', 'selectedField', 'required'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'options'];
+  expandedElement!: FormFormat | null;
 
   constructor() {
     this.formList = this.formService.getAllFormDetails();
@@ -20,5 +39,9 @@ export class FormListComponent implements OnInit {
     this.formService.formList$.subscribe((updatedFormList) => {
       this.formList = updatedFormList;
     });
+  }
+
+  capitalizeFirstLetter(s: string) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
   }
 }
